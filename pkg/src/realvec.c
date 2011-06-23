9a0d1beb07ade6201cd754aec89eecc4ca8034e7
tplate (Tony Plate) <tplate@acm.org>
2011-06-23 03:47:34 +0000

    store length in the vector itself
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/vsa@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/realvec.c
+++ pkg/src/realvec.c
@@ -15,18 +15,33 @@
 /* #include <R_ext/RS.h>     for F77_CALL */
 #include <R_ext/Applic.h> /* for dgemm */
 
+/*
+ * realvec is an external-pointer vector of floats.
+ * Three operations:
+ *   (1) create
+ *   (2) get (sub vector)
+ *   (3) set (sub vector)
+ * The length of the vector is stored as an integer
+ * at the start. The data starts 2 floats into the memory
+ * (to allow for the unlikely possibility that an integer
+ * takes twice as many bytes as a float).
+ */
 static void realvec_finalizer(SEXP ptr)
 {
-    if (!R_ExternalPtrAddr(ptr))
+    float *mem = R_ExternalPtrAddr(ptr);
+    if (!mem)
         return;
-    /* Free(ptr); */
+    Free(mem);
     R_ClearExternalPtr(ptr);
 }
 
 SEXP realvec_create(SEXP n) {
     float *p = 0;
+    int *i;
     SEXP ptr;
-    p = Calloc(INTEGER(n)[0], float);
+    p = Calloc(INTEGER(n)[0]+2, float);
+    i = (int *) p;
+    *i = INTEGER(n)[0];
     ptr = R_MakeExternalPtr(p, install("vecptr"), R_NilValue);
     PROTECT(ptr);
     R_RegisterCFinalizerEx(ptr, realvec_finalizer, TRUE);
@@ -34,35 +49,46 @@ SEXP realvec_create(SEXP n) {
     return ptr;
 }
 
-SEXP realvec_get(SEXP ptr, SEXP veclen, SEXP vecidx) {
-    SEXP ans;
-    float *mem = (float*) R_ExternalPtrAddr(ptr);
-    double *x;
-    int n = INTEGER(veclen)[0];
-    int k = INTEGER(vecidx)[0] - 1; /* vecidx is 1-based, k is 0-based */
-    if (k >= n)
-        error("vecidx too large");
-    if (k < 0)
-        error("vecidx must be +ve (is 1-based)");
-    ans = allocVector(REALSXP, 1);
-    REAL(ans)[0] = *(mem+k);
+/*
+ * ptr is a vector of floats
+ * ptrlen is the length of ptr
+ * idx is a vector of integers (1-based)
+ * idxlen is the length of idx
+ */
+SEXP realvec_get(SEXP ptr, SEXP idx) {
+    float *mem = ((float*) R_ExternalPtrAddr(ptr)) + 2;
+    int ptrlen = *((int*) R_ExternalPtrAddr(ptr));
+    int *idxp = INTEGER(idx); /* vecidx is 1-based, k is 0-based */
+    int i, k = length(idx);
+    SEXP ans = allocVector(REALSXP, k);
+    for (i = 0; i < k; i++) {
+	if (idxp[i] > ptrlen)
+	    error("idx too large");
+	if (idxp[i] < 1)
+	    error("vecidx must be +ve (is 1-based)");
+	REAL(ans)[i] = *(mem+idxp[i]-1);
+    }
     return ans;
 }
 
-SEXP realvec_set(SEXP ptr, SEXP veclen, SEXP vecidx, SEXP vec) {
-    float *mem = (float*) R_ExternalPtrAddr(ptr);
-    double x;
-    int n = INTEGER(veclen)[0];
-    int k = INTEGER(vecidx)[0] - 1; /* vecidx is 1-based, k is 0-based */
-    SEXP ans;
-    if (k >= n)
-        error("vecidx too large");
-    if (k < 0)
-        error("vecidx must be +ve (is 1-based)");
-    x = REAL(vec)[0];
-    /* vecidx is 0-based */
-    REAL(mem)[0] = x;
-    ans = allocVector(REALSXP, 1);
-    REAL(ans)[0] = x;
-    return ans;
+/*
+ * ptr is a vector of floats
+ * ptrlen is the length of ptr
+ * idx is a vector of integers (1-based)
+ * idxlen is the length of idx
+ * val is a vector of doubles (values to set into ptr)
+ */
+SEXP realvec_set(SEXP ptr, SEXP idx, SEXP val) {
+    float *mem = (float*) R_ExternalPtrAddr(ptr) + 2;
+    int ptrlen = *((int*) R_ExternalPtrAddr(ptr));
+    int *idxp = INTEGER(idx); /* vecidx is 1-based, k is 0-based */
+    int i, k = length(idx);
+    for (i = 0; i < k; i++) {
+	if (idxp[i] > ptrlen)
+	    error("idx too large");
+	if (idxp[i] < 1)
+	    error("vecidx must be +ve (is 1-based)");
+	*(mem + idxp[i] - 1) = REAL(val)[i];
+    }
+    return(val);
 }
